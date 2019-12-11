886ce27dbaec476512cba177c8a4d2cc302725eb
Stella Martin <stellamartin222@gmail.com>
2019-12-11 14:29:03 +0000

    cors allowed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
 const apiRouter = require('./routes/api.js')
 const {customErrors, psqlErrors, internalServerError} = require('./errorHander.js')
 
+app.use(cors())
 
 app.use(express.json())
 app.use('/api', apiRouter)
